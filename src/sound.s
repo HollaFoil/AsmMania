47da2b9e77614edd77accd7e191c7d52db152449
LoomT <kajustaida@gmail.com>
2023-10-21 18:58:04 +0200

    Normalized the audio mixing

--- src/sound.s
+++ src/sound.s
@@ -12,14 +12,26 @@ play_sound_fx:
 
     addq $242, %rdi
     subq $242, %rsi
+    movw $0x7FFF, %r10w # INT_MAX
+    movw $0x8001, %r11w # INT_MIN
 
     # adds the sound waves
     loop:
         movw (%rdi), %r8w
-        sarw $1, %r8w
+        
+        shlw %r8w # double the volume of hit sound
+        jno no_overflow_shifting
+        cmovncw %r10w, %r8w
+        cmovcw %r11w, %r8w
+        no_overflow_shifting:
         movw (%rdx, %rcx, 1), %r9w
-        sarw $1, %r9w
+        #sarw $1, %r9w
         addw %r8w, %r9w
+        jno no_overflow
+        andw $0x8000, %r8w # get the sign bit
+        cmovzw %r10w, %r9w # if sign was 0, then MAX_INT
+        cmovnzw %r11w, %r9w # if sign was 1, then MIN_INT
+        no_overflow:
         movw %r9w, (%rdx, %rcx, 1)
         addq $2, %rcx
         addq $2, %rdi
